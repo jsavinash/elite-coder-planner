3c3fec54d0230182996dd6cd3dc902dd5eceb71c
Nishad <Avinash.Nishad@sony.com>
2023-08-09 23:52:55 +0530

    updated

--- src/competitive-programming/competitive-programming-data.js
+++ src/competitive-programming/competitive-programming-data.js
@@ -1,5 +1,18 @@
-
-function createData(id, problem,
+function createData(
+  id,
+  problem,
+  link,
+  difficulty,
+  dataStructures,
+  algorithms,
+  scenarios,
+  categories,
+  companies,
+  solved
+) {
+  return {
+    id,
+    problem,
     link,
     difficulty,
     dataStructures,
@@ -7,53 +20,69 @@ function createData(id, problem,
     scenarios,
     categories,
     companies,
-    solved) {
-    return {
-        id,
-        problem,
-        link,
-        difficulty,
-        dataStructures,
-        algorithms,
-        scenarios,
-        categories,
-        companies,
-        solved
-    };
+    solved,
+  };
 }
 
 export const competitiveProgrammingEntities = [
-    createData(
-        1,
-        '11. Container With Most Water',
-        'https://leetcode.com/problems/container-with-most-water/',
-        'Medium',
-        ['Array'],
-        ['Custom'],
-        ['Water Container'],
-        ['Two Pointer'],
-        [],
-        true),
-    createData(
-        2,
-        '57. Insert Interval',
-        'https://leetcode.com/problems/insert-interval/',
-        'Easy',
-        ['Array', 'List'],
-        ['NA'],
-        ['Water Container'],
-        [],
-        [],
-        true),
-    createData(
-        3,
-        '1679. Max Number of K-Sum Pairs',
-        'https://leetcode.com/problems/max-number-of-k-sum-pairs/',
-        'Medium',
-        ['Array', "HashMap"],
-        ['NA'],
-        ['Pairs of Element'],
-        [],
-        [],
-        true)
+  createData(
+    1,
+    "11. Container With Most Water",
+    "https://leetcode.com/problems/container-with-most-water/",
+    "Medium",
+    ["Array"],
+    ["Custom"],
+    ["Water Container"],
+    ["Two Pointer"],
+    [],
+    true
+  ),
+  createData(
+    2,
+    "57. Insert Interval",
+    "https://leetcode.com/problems/insert-interval/",
+    "Easy",
+    ["Array", "List"],
+    ["NA"],
+    ["Water Container"],
+    [],
+    [],
+    true
+  ),
+  createData(
+    3,
+    "1679. Max Number of K-Sum Pairs",
+    "https://leetcode.com/problems/max-number-of-k-sum-pairs/",
+    "Medium",
+    ["Array", "HashMap"],
+    ["NA"],
+    ["Pairs of Element"],
+    [],
+    [],
+    true
+  ),
+  createData(
+    4,
+    "797. All Paths From Source to Target",
+    "https://leetcode.com/problems/all-paths-from-source-to-target/",
+    "Medium",
+    ["LinkedList", "AdjecentList"],
+    ["NA"],
+    ["Source to destination"],
+    [],
+    [],
+    true
+  ),
+  createData(
+    5,
+    "1732. Find the Highest Altitude",
+    "https://leetcode.com/problems/find-the-highest-altitude/",
+    "Easy",
+    ["Array"],
+    ["Max of addition of from 0 to n-1 index in array"],
+    [],
+    [],
+    [],
+    true
+  ),
 ];
